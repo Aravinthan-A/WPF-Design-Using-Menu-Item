0265370216456251b7d59d989030cb59f78d1482
Aravinthan <134296054+Aravinthan-A@users.noreply.github.com>
2023-07-06 12:21:04 +0530

    Add files via upload

---
+++ Visual.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace String_Function
+{
+    /// <summary>
+    /// Interaction logic for Visual.xaml
+    /// </summary>
+    public partial class Visual : Window
+    {
+        public Visual()
+        {
+            InitializeComponent();
+        }
+
+     
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+           this.Close();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            txtname.Text = "Welcome Aravinthan";
+        }
+    }
+}
